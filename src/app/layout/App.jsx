1a825ccb02941f51d67cd4c5aca9de57ebb38acc
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-05-23 11:39:18 +0200

    Getting the first structure of the page to work

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
-function App() {
-  return <div>Hello</div>;
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/navBar/NavBar';
+import { Container } from 'semantic-ui-react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
