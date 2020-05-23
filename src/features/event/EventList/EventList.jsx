01a9cc627447adc7739a5101e11d039982f1fa1f
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-05-23 17:44:23 +0200

    Adding CRUD functionalities

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -3,10 +3,18 @@ import EventListItem from './EventListItem';
 
 class EventList extends Component {
   render() {
+    const { events, selectEvent, deleteEvent } = this.props;
     return (
       <React.Fragment>
-        {this.props.events.map((event) => {
-          return <EventListItem key={event.id} event={event} />;
+        {events.map((event) => {
+          return (
+            <EventListItem
+              deleteEvent={deleteEvent}
+              key={event.id}
+              event={event}
+              selectEvent={selectEvent}
+            />
+          );
         })}
       </React.Fragment>
     );
